210880c14d6552718771f5f08c152226f75db7b7
alexis <alexisthual@gmail.com>
2021-11-04 20:04:08 +0100

    fix: remove url param when search is empty

--- src/components/Search.tsx
+++ src/components/Search.tsx
@@ -3,17 +3,17 @@ import React from "react";
 import AppContext from "src/context/AppContext";
 
 const Search: React.FC<unknown> = () => {
-  const { search, setContext } = React.useContext(AppContext);
+  const { search, deleteContextKey, setContext } = React.useContext(AppContext);
 
   return (
     <div className="mt-1 relative rounded-md shadow-sm">
-      <div className="absolute inset-y-0 left-0 pl-3 flex items-center pointer-events-none">
-        <span className="text-gray-500 sm:text-sm">
+      <div className="absolute inset-y-0 left-0 p-2 flex items-center pointer-events-none">
+        <span className="text-gray-500 sm:text-sm h-5 w-5">
           <svg
             xmlns="http://www.w3.org/2000/svg"
-            className="h-6 w-6"
+            className="h-full w-full"
             fill="none"
-            viewBox="0 0 32 32"
+            viewBox="0 0 24 24"
             stroke="currentColor"
           >
             <path
@@ -29,13 +29,17 @@ const Search: React.FC<unknown> = () => {
         type="text"
         name="price"
         id="price"
-        className="focus:ring-indigo-500 focus:border-indigo-500 block w-full pl-7 pr-12 sm:text-sm border-gray-300 rounded-md"
+        className="focus:ring-indigo-500 focus:border-indigo-500 block w-full pl-7 pr-2 sm:text-sm border-gray-300 rounded-md"
         placeholder="Rechercher..."
         onChange={(event: React.ChangeEvent<HTMLInputElement>) => {
           event.preventDefault();
-          setContext({
-            search: event.target.value,
-          });
+          if (event.target.value === ``) {
+            deleteContextKey(`search`);
+          } else {
+            setContext({
+              search: event.target.value,
+            });
+          }
         }}
         value={search}
       />
